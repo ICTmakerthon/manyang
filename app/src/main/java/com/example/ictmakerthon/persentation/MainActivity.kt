4214de4fb46763a98add4e0a59ac1c58640adfa4
yaaan7 <wangpajeon@gmail.com>
2025-11-22 01:30:20 +0900

    i will kill you

--- app/src/main/java/com/example/ictmakerthon/persentation/MainActivity.kt
+++ app/src/main/java/com/example/ictmakerthon/persentation/MainActivity.kt
@@ -4,16 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ictmakerthon.ui.theme.ICTmakerthonTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     topBar = {
                         TopAppBar(
                             title = {
-                                Text("Top App Bar", fontFamily = myFont,)
+                                Text("Top App Bar",)
                             },
                             colors = topAppBarColors(
                                 containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -34,7 +50,7 @@ class MainActivity : ComponentActivity() {
                     },
                     bottomBar = {
                         BottomAppBar {
-                            Text("Bottom App Bar", fontFamily = myFont,)
+                            Text("Bottom App Bar",)
                         }
                     }
                 ) { innerPadding ->
@@ -49,7 +65,26 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Composable
+fun AttendEntry(id: String, name: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier.padding(5.dp)
+    ){
 
+        Spacer(Modifier.width(7.dp))
+        Column{
+            Text(
+                text = id,
+            )
+            Text(
+                text = name,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+
+            )
+        }
+    }
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
